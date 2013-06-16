7f9fbf31bdad760b7eb09d25f71c56e69ab97eac
sinojelly <sinojelly@gmail.com>
2013-06-16 20:30:47 +0800

    modify to compile with google android gradle plugin.

--- src/main/java/coffee/DripCoffeeModule.java
+++ src/main/java/coffee/DripCoffeeModule.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import dagger.Module;
 import dagger.Provides;
 
-@Module(entryPoints = CoffeeActivity_.class, includes = PumpModule.class)
+@Module(injects = CoffeeActivity_.class, includes = PumpModule.class)
 class DripCoffeeModule {
 
 	private final Context context;
